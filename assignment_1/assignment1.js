3ac99e87af3dd1512c15e63b861cebaecdd9d75e
Jin Yoo <jyoo@BrainStations-MacBook-Air-26.local>
2017-01-31 16:52:55 -0500

    first commit

---
+++ assignment_1/assignment1.js
@@ -0,0 +1,53 @@
+/*
+Exercise #1
+Initialize an empty array below in the variable numbers.
+*/
+var numbers;
+
+
+/*
+Exercise #2
+Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
+*/
+
+for(var i=0;i<10;i++){
+    //use the array push() method here.
+}
+
+
+/*
+Exercise #3
+Test that you used the array push method correctly by printing the sum of the values at position 3 and 6 of the array (use the console.log() function to print to the console). 
+The outputted value should be 9.
+*/
+
+
+
+
+/*
+Exercise #4
+Use the console.log() function and the array .pop() to add all the values in the array.
+*/
+
+var sum = 0;
+/* remove this line to uncomment the code block
+while(numbers.length != 0){
+    //write pop function here
+}
+remove this line to uncomment the code block */
+console.log(sum);
+
+
+/*
+Exercise #5
+Create an empty object and assign it the variable car below.
+*/
+var car;
+
+/*
+Exercise #5
+Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputing property make to the console. You should see the value "black" printed out.
+*/
+
+
+
---
+++ assignment_2/assignment2.js
@@ -0,0 +1,19 @@
+/*
+Exercise #1
+Create a for loop that prints out the numbers 1 to 100 in the console.
+*/
+
+
+
+/*
+Exercise #2
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
